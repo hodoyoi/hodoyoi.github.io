565157920966710f8d6b6982a79f9d13dd995715
gen0e0 <iezaki@hodoyoi.net>
2020-11-04 23:22:37 +0900

    add top layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
-  .container
-    Logo
-    h1.title hodoyoi.net
-    b-btn(variant="primary") Button
+  .container.text-center
+    h1.pixel10.mt-5 シンプルで<br/>かんたんで<br/>小さいプログラムを作る
+    Logo.mt-5
+    h2.pixel10.mt-5 ほどよい合同会社
 
+    // b-btn(variant="primary") Button
 </template>
 
 <script lang="ts">
